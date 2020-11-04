ccecccc1a021f055085efea67da67c3a23c85e19
Allen-TaiLin <skygary2000@gmail.com>
2020-11-04 20:11:11 +0800

    Add app.js and package file

---
+++ app.js
@@ -0,0 +1,43 @@
+// require packages used in the projects
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+
+// routes setting
+app.get('/', (req, res) => {
+
+  // past the restaurant data into 'index' partial template
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurantID = req.params.restaurant_id
+  const restaurant = restaurantList.results.find((item) => {
+    return item.id.toString() === restaurantID
+  })
+
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter((item) => {
+    return item.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
